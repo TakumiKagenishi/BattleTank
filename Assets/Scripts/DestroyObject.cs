074fefe3c40168de7dd0aef113bdea8c2332edb1
Takumi-Kagenishi <rip.mymanzzz@gmail.com>
2020-12-05 15:49:59 +0900

    敵に得点をつけて、画面にスコアを表示させた。

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -9,6 +9,11 @@ public class DestroyObject : MonoBehaviour
     [SerializeField]
     private GameObject effectPrefab2;
     public int ofjectHP;
+    [SerializeField]
+    private GameObject[] itemPrefabs;
+    [SerializeField]
+    private int scoreValue;
+    private ScoreManager sm;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -28,13 +33,20 @@ public class DestroyObject : MonoBehaviour
                 GameObject effect2 = Instantiate(effectPrefab2, other.transform.position, Quaternion.identity);
                 Destroy(effect2, 2.0f);
                 Destroy(this.gameObject);
+                int itemNumber = Random.Range(0, itemPrefabs.Length);
+                Vector3 pos = transform.position;
+                if (itemPrefabs.Length != 0)
+                {
+                    Instantiate(itemPrefabs[itemNumber], new Vector3(pos.x, pos.y + 0.6f, pos.z), Quaternion.identity);
+                }
+                sm.AddScore(scoreValue);
             }
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        sm = GameObject.Find("ScoreLabel").GetComponent<ScoreManager>();
     }
 
     // Update is called once per frame
--- Assets/Scripts/EnemyShotShell.cs
+++ Assets/Scripts/EnemyShotShell.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyShotShell : MonoBehaviour
 {
@@ -10,17 +9,27 @@ public class EnemyShotShell : MonoBehaviour
     [SerializeField]
     private AudioClip shotSound;
     private int interval;
+    public float stopTimer = 5.0f;
+    [SerializeField]
+    private Text stopLabel;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         interval += 1;
-        if(interval % 150 == 0)
+        stopTimer -= Time.deltaTime;
+        if (stopTimer < 0)
+        {
+            stopTimer = 0;
+        }
+        stopLabel.text = "" + stopTimer.ToString("0");
+
+        if (interval % 100 == 0 && stopTimer <= 0)
         {
             GameObject enemyShell = Instantiate(enemyShellPrefab, transform.position, Quaternion.identity);
             Rigidbody enemyShellRb = enemyShell.GetComponent<Rigidbody>();
@@ -28,5 +37,11 @@ public class EnemyShotShell : MonoBehaviour
             AudioSource.PlayClipAtPoint(shotSound, transform.position);
             Destroy(enemyShell, 3.0f);
         }
+    }
+
+    public void AddStopTimer(float amount)
+    {
+        stopTimer += amount;
+        stopLabel.text = "" + stopTimer.ToString("0");
     }
 }
