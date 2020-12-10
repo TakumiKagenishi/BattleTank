42389135297f392b2298938217374a1185e74d94
Takumi-Kagenishi <rip.mymanzzz@gmail.com>
2020-12-10 16:14:18 +0900

    戦闘ヘリの攻撃追加

--- Assets/Scripts/BattleHeli.cs
+++ Assets/Scripts/BattleHeli.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleHeri : MonoBehaviour
+public class BattleHeli : MonoBehaviour
 {
+    public GameObject target;
+    public GameObject enemyShotShell;
+    public GameObject enemyShellPrefab;
+    private int count = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,24 @@ public class BattleHeri : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(target.transform);
+        if(Vector3.Distance(transform.position, target.transform.position) > 10f)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 3);
+        }
+        else
+        {
+            if(transform.position.y < 8f)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 3);
+            }
+            count += 1;
+            if(count % 300 == 0)
+            {
+                GameObject enemyShell = Instantiate(enemyShellPrefab, enemyShotShell.transform.position, enemyShotShell.transform.rotation);
+                Rigidbody enemyShellRb = enemyShell.GetComponent<Rigidbody>();
+                enemyShellRb.AddForce(transform.forward * 100);
+            }
+        }
     }
 }
