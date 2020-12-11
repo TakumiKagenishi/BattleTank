99fb07924c736511ec3fa7a3e14f42a49b241318
Takumi-Kagenishi <rip.mymanzzz@gmail.com>
2020-12-11 13:08:42 +0900

    オリジナルアイテム開発途中

---
+++ Assets/Scripts/SpeedUpItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpItem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject effectPrefab;
+    [SerializeField]
+    private AudioClip getSound;
+    private TankMovement tm;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log(other.gameObject.tag);
+            tm = GameObject.Find("Tank").GetComponent<TankMovement>();
+            tm.moveSpeed = 6;
+            tm.turnSpeed = 80;
+            Destroy(gameObject);
+            GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
+            Destroy(effect, 0.5f);
+            AudioSource.PlayClipAtPoint(getSound, transform.position);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/StopAttackItem.cs
+++ Assets/Scripts/StopAttackItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StopAttackItem : MonoBehaviour
+public class StopAttackItem : MonoBehaviour 
 {
     private GameObject[] targets;
     [SerializeField]
